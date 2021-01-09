5b5e7e66dcc1b8ff0459f9e5ec2b567795d9b576
Dougl <Element1000@!#$>
2021-01-09 22:33:57 +0100

    09-01-2021 : Afgeronde inlog pagina en registratie pagina.

--- www/assets/js/pages/Index.js
+++ www/assets/js/pages/Index.js
@@ -5,13 +5,12 @@ window.onload = function(){
     document.getElementById('signupbutton').onclick = function(){ //een onclick event word gemaakt op de signupbutton
 
         var inputfields = document.querySelectorAll("input");
-        console.log(inputfields);
 
         for(let i = 0; i < inputfields.length; i++){
             if(inputfields[i].value.length == 0){
                 const feedbackObject = document.getElementById('feedback');
                 geefFeedback(feedbackObject,"Alle tekst vakken moeten worden ingevuld",5000);
-                break;
+                return;
             }
         }
 
@@ -23,29 +22,37 @@ window.onload = function(){
         */
         FYSCloud.API.queryDatabase(
 
-            "SELECT * FROM profiel WHERE email = ? and wachtwoord = ?",
-            [emailInput, passwordInput]
+            "SELECT * FROM profiel WHERE email = ?",
+            [emailInput]
         ).done(function(data) {
 
         //Als de gebruiker informatie invult zonder dat het in de database bekent is krijgt hij/zij een fout melding
-        // data.leght mag ook worden geschreven als | data.lenght > 1 omdat je soms meer data terug kan krijgen dan 1
+        // data.length mag ook worden geschreven als | data.lenght > 1 omdat je soms meer data terug kan krijgen dan 1
             if (data.length == 1){
-
-                FYSCloud.Session.set("gebruikersId", data[0].gebruikersid);
-
-                FYSCloud.URL.redirect("reiskeuze.html"); //gebruiker word verwezen naar de reiskeuze pagina.
-
+                if(data.wachtwoord == passwordInput){
+                    FYSCloud.Session.set("gebruikersId", data[0].gebruikersid);
+                    FYSCloud.URL.redirect("reiskeuze.html"); //gebruiker word verwezen naar de reiskeuze pagina.
+                }else{
+                    const feedbackObject = document.getElementById('feedback');
+                    document.getElementById('wachtwoordvergetenlink').style.display = "inline-block";
+                    setTimeout(function() { VerwijderHref(document.getElementById('wachtwoordvergetenlink')); }, 10000);
+                    geefFeedback(feedbackObject,"Onjuist wachtwoord, wachtwoord vergeten?",10000);
+                }
             } else
                 {
-                document.getElementById("debugmessage").innerHTML = "verkeerde informatie ingevuld probeer opnieuw";
+                    const feedbackObject = document.getElementById('feedback');
+                    geefFeedback(feedbackObject,"Onjuiste informatie, profiel bestaat niet.",5000);
 
             }
+
         }).fail(function(reason) {
             console.log(reason);
         });
 
+    }
 
-
+    function VerwijderHref(ahref){
+        ahref.style.display = "none";
     }
 
 };
--- www/assets/js/pages/Registratie.js
+++ www/assets/js/pages/Registratie.js
@@ -3,32 +3,54 @@ window.onload = function(){
     document.getElementById('registerbtn').onclick = function() {
 
       var inputfields = document.querySelectorAll("input");
-      console.log(inputfields);
 
-      for(let i = 0; i < inputfields.length; i++){
+          for(let i = 0; i < inputfields.length; i++){
           if(inputfields[i].value.length == 0){
-              const feedbackObject = document.getElementById('feedBack');
+              const feedbackObject = document.getElementById('feedback');
               geefFeedback(feedbackObject,"Alle tekst vakken moeten worden ingevuld",5000);
-              break;
+              return;
           }
       }
+        if (wachtwoordbtn.value != herhaalWachtwoord.value ){
+            const feedbackObject = document.getElementById('feedback');
+            geefFeedback(feedbackObject,"Wachtwoord komt niet overeen",5000);
+            return;
+        }
 
         var emailInput = document.getElementById("emailbtn").value; //id word verbonden met het atribuut
         var passwordInput = document.getElementById("wachtwoordbtn").value; //id word verbonden met het atribuut
         var voornaamInput = document.getElementById("voornaam").value; //id word verbonden met het atribuut
         var achternaamInput = document.getElementById("achternaam").value; //id word verbonden met het atribuut
-        var geslachtInput = document.getElementById("geslachtbtn").value; //id word verbonden met het atribuut
+        var geslachtInput = document.getElementById("geslacht").value; //id word verbonden met het atribuut
+
 
 
         //database atribturen worden opgehaald.
         FYSCloud.API.queryDatabase(
-            "INSERT INTO `profiel` (`wachtwoord`,`voornaam`,`achternaam`, `email` ) VALUES (?, ?, ?, ?)",
-            [passwordInput, voornaamInput, achternaamInput, emailInput]
+            "SELECT * FROM profiel WHERE email =?",
+            [emailInput]
         ).done(function (data) {
-            console.log(data)
-        }).fail(function (reason) {
-         console.log(reason);
+
+            console.log(data);
+            if (data.length > 0){
+                const feedbackObject = document.getElementById('feedback');
+                geefFeedback(feedbackObject,"De email bestaat al, probeer een andere.",5000);
+                return;
+            }
+            FYSCloud.API.queryDatabase(
+                "INSERT INTO `profiel` (`wachtwoord`,`voornaam`,`achternaam`, `email`, `geslacht` ) VALUES (?, ?, ?, ?, ?)",
+                [passwordInput, voornaamInput, achternaamInput, emailInput, geslachtInput]
+            ).done(function (data) {
+                FYSCloud.URL.redirect("Index.html"); //gebruiker word verwezen naar de inlog pagina.
+
+            }).fail(function (reason) {
+                console.log(reason);
             });
+
+        }).fail(function (reason) {
+            console.log(reason);
+        });
+
 
     }
 
