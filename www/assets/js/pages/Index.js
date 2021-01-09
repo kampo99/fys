c9e7429a6c55bf2395614a57e3f5c903ca9ad9e5
Dougl <Element1000@!#$>
2021-01-09 02:06:17 +0100

    09-01-2021 : Feedback op inlog pagina en registratie pagina.

--- www/assets/js/pages/Index.js
+++ www/assets/js/pages/Index.js
@@ -1,22 +1,39 @@
 window.onload = function(){
-    
-    document.getElementById('signupbutton').onclick = function(){
-
-        var emailInput = document.getElementById("email").value;
-        var passwordInput = document.getElementById("psw").value;
 
 
+
+    document.getElementById('signupbutton').onclick = function(){ //een onclick event word gemaakt op de signupbutton
+
+        var inputfields = document.querySelectorAll("input");
+        console.log(inputfields);
+
+        for(let i = 0; i < inputfields.length; i++){
+            if(inputfields[i].value.length == 0){
+                const feedbackObject = document.getElementById('feedback');
+                geefFeedback(feedbackObject,"Alle tekst vakken moeten worden ingevuld",5000);
+                break;
+            }
+        }
+
+        var emailInput = document.getElementById("email").value; //invoer balk emailInput word verbonden aan atribuut email
+        var passwordInput = document.getElementById("psw").value;//invoer balk passwordInput word verbonden aan atribuut psw
+
+        /*selecteer de tabel profiel en de atributen email en wachtwoord.
+          Die worden geassigend aan de id's van email en wachtwoord in de index.
+        */
         FYSCloud.API.queryDatabase(
 
             "SELECT * FROM profiel WHERE email = ? and wachtwoord = ?",
             [emailInput, passwordInput]
         ).done(function(data) {
 
+        //Als de gebruiker informatie invult zonder dat het in de database bekent is krijgt hij/zij een fout melding
+        // data.leght mag ook worden geschreven als | data.lenght > 1 omdat je soms meer data terug kan krijgen dan 1
             if (data.length == 1){
 
                 FYSCloud.Session.set("gebruikersId", data[0].gebruikersid);
 
-                FYSCloud.URL.redirect("reiskeuze.html");
+                FYSCloud.URL.redirect("reiskeuze.html"); //gebruiker word verwezen naar de reiskeuze pagina.
 
             } else
                 {
--- www/assets/js/pages/Registratie.js
+++ www/assets/js/pages/Registratie.js
@@ -1,12 +1,26 @@
 window.onload = function(){
 
     document.getElementById('registerbtn').onclick = function() {
-        var emailInput = document.getElementById("emailbtn").value;
-        var passwordInput = document.getElementById("wachtwoordbtn").value;
-        var voornaamInput = document.getElementById("voornaam").value;
-        var achternaamInput = document.getElementById("achternaam").value;
-        var geslachtInput = document.getElementById("geslachtbtn").value;
 
+      var inputfields = document.querySelectorAll("input");
+      console.log(inputfields);
+
+      for(let i = 0; i < inputfields.length; i++){
+          if(inputfields[i].value.length == 0){
+              const feedbackObject = document.getElementById('feedBack');
+              geefFeedback(feedbackObject,"Alle tekst vakken moeten worden ingevuld",5000);
+              break;
+          }
+      }
+
+        var emailInput = document.getElementById("emailbtn").value; //id word verbonden met het atribuut
+        var passwordInput = document.getElementById("wachtwoordbtn").value; //id word verbonden met het atribuut
+        var voornaamInput = document.getElementById("voornaam").value; //id word verbonden met het atribuut
+        var achternaamInput = document.getElementById("achternaam").value; //id word verbonden met het atribuut
+        var geslachtInput = document.getElementById("geslachtbtn").value; //id word verbonden met het atribuut
+
+
+        //database atribturen worden opgehaald.
         FYSCloud.API.queryDatabase(
             "INSERT INTO `profiel` (`wachtwoord`,`voornaam`,`achternaam`, `email` ) VALUES (?, ?, ?, ?)",
             [passwordInput, voornaamInput, achternaamInput, emailInput]
@@ -17,6 +31,8 @@ window.onload = function(){
             });
 
     }
+
+    //password hasen en laten zien met een checkbox functie.
         document.getElementById('checkbox').onclick = function () {
             console.log("Ik heb geklikt")
             var x = document.getElementById("wachtwoordbtn");
@@ -26,14 +42,5 @@ window.onload = function(){
                 x.type = "password";
             }
         }
-
-    function toSqlDatetime(inputDate) {
-        const date = new Date(inputDate);
-        const dateWithOffset = new Date(date.getTime() - (date.getTimezoneOffset() * 60000));
-        return dateWithOffset
-            .toISOString()
-            .slice(0, 19)
-            .replace("T", " ");
-    }
 
 };
