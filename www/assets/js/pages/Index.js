93e77f11e14232feedd0f62e387e65fbf9f92133
Dougl <Element1000@!#$>
2021-01-10 20:50:48 +0100

    09-01-2021 : Kleine foutjes gecorrigeerd

--- www/assets/js/pages/Index.js
+++ www/assets/js/pages/Index.js
@@ -15,7 +15,7 @@ window.onload = function(){
         }
 
         var emailInput = document.getElementById("email").value; //invoer balk emailInput word verbonden aan atribuut email
-        var passwordInput = document.getElementById("psw").value;//invoer balk passwordInput word verbonden aan atribuut psw
+        var passwordInput = document.getElementById("wachtwoord").value;//invoer balk passwordInput word verbonden aan atribuut psw
 
         /*selecteer de tabel profiel en de atributen email en wachtwoord.
           Die worden geassigend aan de id's van email en wachtwoord in de index.
@@ -25,11 +25,10 @@ window.onload = function(){
             "SELECT * FROM profiel WHERE email = ?",
             [emailInput]
         ).done(function(data) {
-
         //Als de gebruiker informatie invult zonder dat het in de database bekent is krijgt hij/zij een fout melding
         // data.length mag ook worden geschreven als | data.lenght > 1 omdat je soms meer data terug kan krijgen dan 1
             if (data.length == 1){
-                if(data.wachtwoord == passwordInput){
+                if(data[0].wachtwoord == passwordInput){
                     FYSCloud.Session.set("gebruikersId", data[0].gebruikersid);
                     FYSCloud.URL.redirect("reiskeuze.html"); //gebruiker word verwezen naar de reiskeuze pagina.
                 }else{
